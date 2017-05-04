3806618d0ae3e6df88fa58dd82b1d18fa0c1fec3
MathiasM <mamu@step.dk>
2017-05-04 10:47:48 +0200

    Added debugging help. Use arrowkeys and pgUp/-down to move the planet around in the scene

--- js/utillities/eventhandler.js
+++ js/utillities/eventhandler.js
@@ -3,5 +3,25 @@ var eventhandler = {
   // Simple test event!
   sayhi: function(){
     console.log('Hi!');
-  }
+  },
+  'movePlanet': {
+    up: function(){
+      planet.threejsObj.position.y += 0.5;
+    },
+    right: function(){
+      planet.threejsObj.position.x += 0.5;
+    },
+    down: function(){
+      planet.threejsObj.position.y -= 0.5;
+    },
+    left: function(){
+      planet.threejsObj.position.x -= 0.5;
+    },
+    away: function(){
+      planet.threejsObj.position.z -= 0.5;
+    },
+    closer: function(){
+      planet.threejsObj.position.z += 0.5;
+    },
+  },
 };
--- js/utillities/keyhandler.js
+++ js/utillities/keyhandler.js
@@ -2,12 +2,20 @@
 // Key handler handles keyboard inputs and can map these to the eventhandler or WAHTEVER
 var keyhandler = {
     keymap: {
-      's': eventhandler.sayhi, // Say "Hi" when the 's' key is pressed
+      's':          eventhandler.sayhi, // Say "Hi" when the 's' key is pressed
+      'ArrowUp':    eventhandler.movePlanet.up,
+      'ArrowRight': eventhandler.movePlanet.right,
+      'ArrowDown':  eventhandler.movePlanet.down,
+      'ArrowLeft':  eventhandler.movePlanet.left,
+      'PageUp':     eventhandler.movePlanet.away,
+      'PageDown':   eventhandler.movePlanet.closer,
     },
     // On the keydown event - Trigger a key event if it is mapped to something in this.keymap
     keyevent: function(key){
       if(this.keymap[key]){
-        this.keymap[key](); // Fire the keymaps function
+        this.keymap[key]();   // Fire the keymaps function
+      }else{
+        console.log('Pressed "'+key+'" but nothing is mapped to it.');
       }
     },
 };
